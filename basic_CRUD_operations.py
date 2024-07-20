a4feee6997eceb01728e4e480bfb4b900356c570
KViolinov <152738737+KViolinov@users.noreply.github.com>
2024-07-20 16:50:22 -0700

    Update and rename connectionToFirebase.py to basic_CRUD_operations.py

--- basic_CRUD_operations.py
+++ basic_CRUD_operations.py
@@ -15,9 +15,30 @@ firebase = pyrebase.initialize_app(config)
 database = firebase.database()
 
 data = {
-        "Age": 21,
-        "Name": "Gosho",
-        "Likes Pizza": True
+        "Age": 12,
+        "Name": "bulka",
+        "Likes Pizza": False
         }
 
+
+# add/create data
+
 database.push(data)
+database.child("Users").child("FirstPerson").set(data)
+
+
+# print/read data
+
+bulka = database.child("Users").child("FirstPerson").get()
+print(bulka.val())
+
+
+# update data
+
+database.child("Users").child("FirstPerson").update({"Name": "John"})
+
+
+# delete data
+
+database.child("Users").child("FirstPerson").child("Age").remove()
+database.child("Users").child("FirstPerson").remove()
