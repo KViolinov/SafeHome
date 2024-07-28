60e1abb33f214cb84d9b4a4e5a8cd85065c66b07
KViolinov <152738737+KViolinov@users.noreply.github.com>
2024-07-29 01:42:25 +0300

    Update workingESPtoFirebaseCode.cpp

--- workingESPtoFirebaseCode.cpp
+++ workingESPtoFirebaseCode.cpp
@@ -1,27 +1,36 @@
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 
-// Replace these with your network credentials
+// Network credentials
 #define WIFI_SSID "B-Smart"
 #define WIFI_PASSWORD "0889909595"
 
-// Replace these with your Firebase project credentials
+// Firebase project credentials
 #define FIREBASE_HOST "https://safehome-c4576-default-rtdb.firebaseio.com"
 #define FIREBASE_API_KEY "AIzaSyCO56LE4nFc4Th3WDbt_uSiXbeNiKKlouI"
 
-// Replace these with your Firebase Authentication credentials
+// Firebase Authentication credentials
 #define USER_EMAIL "kviolinov@gmail.com"
 #define USER_PASSWORD "Kv0889909595"
-// Create an instance of the FirebaseData class
+
+// Createing an instance of the FirebaseData class
 FirebaseData firebaseData;
 FirebaseAuth auth;
 FirebaseConfig config;
 
+String getMacAddress() {
+  uint8_t mac[6];
+  char macStr[18];
+  WiFi.macAddress(mac);
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  return String(macStr);
+}
+
 void setup() {
-  // Start the serial communication
   Serial.begin(115200);
 
-  // Connect to Wi-Fi
+  // Connecting to Wi-Fi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -41,7 +50,7 @@ void setup() {
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 
-  // Try to sign in the user
+  // Trying to sign in the user
   if (Firebase.signUp(&config, &auth, USER_EMAIL, USER_PASSWORD)) {
     Serial.println("Sign up succeeded");
   } else {
@@ -57,8 +66,13 @@ void loop() {
   // Generate a random integer
   int randomNumber = random(0, 100); // Random number between 0 and 99
 
+  String macAddress = getMacAddress();
+
+  // Create the Firebase path using the MAC address
+  String firebasePath = "/" + macAddress + "/randomNumber";
+
   // Send the random number to Firebase
-  if (Firebase.ready() && Firebase.setInt(firebaseData, "/randomNumber", randomNumber)) {
+  if (Firebase.ready() && Firebase.setInt(firebaseData, firebasePath.c_str(), randomNumber)) {
     Serial.println("Data sent successfully");
   } else {
     Serial.print("Failed to send data: ");
