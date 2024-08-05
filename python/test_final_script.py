db5b204126b1fcaade2096f982055f6d868d0c7e
KViolinov <152738737+KViolinov@users.noreply.github.com>
2024-08-05 08:29:11 -0700

    Update test_final_script.py

--- python/test_final_script.py
+++ python/test_final_script.py
@@ -5,17 +5,7 @@ import pyrebase
 import time
 
 # Firebase configuration
-config = {
-    "apiKey": "AIzaSyCO56LE4nFc4Th3WDbt_uSiXbeNiKKlouI",
-    "authDomain": "safehome-c4576.firebaseapp.com",
-    "projectId": "safehome-c4576",
-    "databaseURL": "https://safehome-c4576-default-rtdb.firebaseio.com",
-    "storageBucket": "safehome-c4576.appspot.com",
-    "messagingSenderId": "516133592519",
-    "appId": "1:516133592519:web:0a8b1755b92d70877bcde2",
-    "measurementId": "G-TXJSM010BL",
-    "serviceAccount": "firebase/serviceAccount.json"
-}
+config = { }
 
 # Connecting to Firebase
 try:
