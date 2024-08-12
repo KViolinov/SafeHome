964b88ec8a0cbec368036bedd1220a625bc5193a
KViolinov <152738737+KViolinov@users.noreply.github.com>
2024-08-13 00:52:02 +0300

    Update and rename python/Firebase_CRUD_operations.py to python/testing/Firebase_CRUD_operations.py

--- python/testing/Firebase_CRUD_operations.py
+++ python/testing/Firebase_CRUD_operations.py
@@ -1,15 +1,6 @@
 import pyrebase
 
-config = {
-    "apiKey": "AIzaSyCO56LE4nFc4Th3WDbt_uSiXbeNiKKlouI",
-    "authDomain": "safehome-c4576.firebaseapp.com",
-    "projectId": "safehome-c4576",
-    "databaseURL": "https://safehome-c4576-default-rtdb.firebaseio.com",
-    "storageBucket": "safehome-c4576.appspot.com",
-    "messagingSenderId": "516133592519",
-    "appId": "1:516133592519:web:0a8b1755b92d70877bcde2",
-    "measurementId": "G-TXJSM010BL"
-}
+config = { }
 
 firebase = pyrebase.initialize_app(config)
 database = firebase.database()
